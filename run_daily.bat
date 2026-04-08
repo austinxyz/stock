e0fce85d7f29d9e79e8f0203c3bb88b44fc3ff25
austinxyz <austinxyz@users.noreply.github.com>
2026-04-08 07:46:22 -0700

    chore: remove FIG from run_daily.bat (position closed)

--- run_daily.bat
+++ run_daily.bat
@@ -7,6 +7,4 @@ py analysis\oklo_score.py
 timeout /t 5 /nobreak >nul
 py analysis\ebay_score.py
 timeout /t 5 /nobreak >nul
-py analysis\fig_score.py
-timeout /t 5 /nobreak >nul
 py analysis\kweb_score.py
